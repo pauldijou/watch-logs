c867563ce5c8e75ed85f578033f5fcd1ab254106
Paul Dijou <paul.dijou@gmail.com>
2016-10-28 17:47:10 +0200

    Improve rendering

--- renderer.js
+++ renderer.js
@@ -1,4 +1,4 @@
-const { app, globalShortcut } = require('electron').remote;
+// const { app } = require('electron').remote;
 const { state, logs, updateUI } = require('./src/state');
 const { init: initWatcher } = require('./src/watcher');
 const { init: initSpy } = require('./src/spy');
@@ -8,9 +8,6 @@ initWatcher();
 
 require('./src/render');
 
-// app.on('ready', function () {
-//
-// });
 
 // app.on('browser-window-blur', () => {
 //   updateUI({ lastFocus: new Date() });
